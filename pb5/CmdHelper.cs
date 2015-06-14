0a5c079b50e8c837b019de4befcb3b10ed2ef860
Boldijar Paul <paul.bv96@yahoo.com>
2015-06-14 16:09:34 +0300

    added

---
+++ pb5/CmdHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pb5
+{
+    class CmdHelper
+    {
+        public static void ExecuteCmd(String cmd)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C " + cmd;
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
